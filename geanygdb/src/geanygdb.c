56b09670e2f0ff3cd7dcd8bfcfae298a11c4696a
Dominic Hopf <dmaphy@googlemail.com>
2010-01-02 22:53:35 +0000

    update copyright information
    git-svn-id: https://geany-plugins.svn.sourceforge.net/svnroot/geany-plugins/trunk/geany-plugins@1104 e8383189-8249-0410-b506-acc975200cbd

--- geanygdb/src/geanygdb.c
+++ geanygdb/src/geanygdb.c
@@ -1,13 +1,12 @@
 /*
  * geanygdb.c - Integrated debugger plugin for the Geany IDE
  * Copyright 2008 Jeff Pohlmeyer <yetanothergeek(at)gmail(dot)com>
- *
+ * Copyright 2009 - 2010 Dominic Hopf <dmaphy@googlemail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- *
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,9 +15,7 @@
  *
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- *
- *
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA. *
  */
 
 
