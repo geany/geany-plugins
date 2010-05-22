16b4b65d045b5df53f571a86f8e6a7a7b648ea4c
Colomban Wendling <ban@herbesfolles.org>
2010-05-22 23:19:51 +0000

    GeanyGenDoc: Remove some obsolete files
    Drop some leftover files that was obsoleted by the import in
    geany-plugins.
    
    
    git-svn-id: https://geany-plugins.svn.sourceforge.net/svnroot/geany-plugins/trunk/geany-plugins@1383 e8383189-8249-0410-b506-acc975200cbd

--- geanygendoc/autogen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-
-mkdir -p build/aux          || exit 1
-mkdir -p build/m4           || exit 1
-#~ gtkdocize --flavour no-tmpl || exit 1
-glib-gettextize -c -f       || exit 1
-autoreconf -vfi             || exit 1
