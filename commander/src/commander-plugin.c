43081f06a9ee95d281bd54ab76b8767877eae5eb
Colomban Wendling <ban@herbesfolles.org>
2012-08-06 18:17:39 +0200

    commander: Fix the Geany API dependency
    We need API 205 for plugin_idle_add().

--- commander/src/commander-plugin.c
+++ commander/src/commander-plugin.c
@@ -32,7 +32,7 @@ GeanyPlugin      *geany_plugin;
 GeanyData        *geany_data;
 GeanyFunctions   *geany_functions;
 
-PLUGIN_VERSION_CHECK(195)
+PLUGIN_VERSION_CHECK(205)
 
 PLUGIN_SET_TRANSLATABLE_INFO (
   LOCALEDIR, GETTEXT_PACKAGE,
