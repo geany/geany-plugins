37e5a68c1bbc9c75df758d6b20eec0140474f3cf
mjnovice <mayank25080562@gmail.com>
2013-02-21 17:03:15 +0530

    Update geanyspoj.c

--- geanyspoj.c
+++ geanyspoj.c
@@ -4,16 +4,14 @@
 GeanyPlugin     *geany_plugin;
 GeanyData       *geany_data;
 GeanyFunctions  *geany_functions;
-GeanyDocument   *yof;
+GeanyDocument 	*soln_doc;
 PLUGIN_VERSION_CHECK(211)
 
 PLUGIN_SET_INFO("SPOJ Submissions", "Tool to submit spoj problem submissions from geany itself!",
                 "1.0", "Mayank Jha <mayank25080562@gmail.com>");
-static GtkWidget *ii;
-static GtkWidget *window;
-static GtkWidget *frame;
-static GtkWidget *main_menu_item = NULL;
 
+static GtkWidget *window;
+static GtkWidget *main_menu_item = NULL;
 static GtkWidget *user_label;
 static GtkWidget *pwd_label;
 static GtkWidget *probid_label;
@@ -27,7 +25,7 @@ static GtkWidget *probid_entry;
 static GtkWidget *language_box;
 static GtkWidget *server_box;
 static GtkComboBox *combo_box;
-GList *glist=NULL;
+
 
 static GtkWidget *submit_button;
 static GtkWidget *save_button;
@@ -37,91 +35,118 @@ static GtkWidget *table;
 static void item_activate_cb(GtkMenuItem *menuitem, gpointer gdata)
 {
    gtk_widget_show_all(window);
-   // dialogs_show_msgbox(GTK_MESSAGE_INFO, "Hello World");
 }
 void spoj_preferences_load_doit(void)
-{gchar bufa[30],bufb[30],bufc[30];
-int thenum;
-GtkWidget *widget;
+{
+	gchar doc_fn[40],user_ent[30],pwd_ent[30],probid_ent[30];
+int active_choice;
+
 FILE *f=fopen("/home/mj/code/datafile.txt","r"); /*the path where the data is stored like username,password etc.*/
-fscanf(f,"%s %s %s %d",bufa,bufb,bufc,&thenum);
-gtk_entry_set_text(GTK_ENTRY(user_entry),(const gchar*)(bufa));
-gtk_entry_set_text(GTK_ENTRY(pwd_entry),(const gchar*)(bufb));
-gtk_entry_set_text(GTK_ENTRY(probid_entry),(const gchar*)(bufc));;
-gtk_combo_box_set_active(GTK_COMBO_BOX(language_box),thenum);
+if(!f)
+{
+
+fscanf(f,"%s %s %s %d",doc_fn,user_ent,pwd_ent,&active_choice);
+gtk_entry_set_text(GTK_ENTRY(user_entry),(doc_fn));
+gtk_entry_set_text(GTK_ENTRY(pwd_entry),(user_ent));
+gtk_entry_set_text(GTK_ENTRY(probid_entry),(probid_ent));;
+gtk_combo_box_set_active(GTK_COMBO_BOX(language_box),active_choice);
+
+}
+else
+{
+//print an error dialog box and then 
+return;	
+}
 }
 
 static void spoj_save_doit(GtkWidget *save_button, gpointer gdata)
 {
 FILE *da=fopen("/home/mj/code/datafile.txt","w");/*the path where the data is stored like username,password etc.*/
-fputs(gtk_entry_get_text(GTK_ENTRY(user_entry)),da);
-fputc(' ',da);
-fputs(gtk_entry_get_text(GTK_ENTRY(pwd_entry)),da);
-fputc(' ',da);
-fputs(gtk_entry_get_text(GTK_ENTRY(probid_entry)),da);
-fputc(' ',da);
-fprintf(da,"%d",gtk_combo_box_get_active(GTK_COMBO_BOX(language_box)));
-//fputs(gtk_entry_get_text(GTK_ENTRY(ui_lookup_widget(ui_widgets.prefs_dialog, "passwordentry1"))),da);
-//fputc(' ',da);
-//puts(gtk_entry_get_text(GTK_ENTRY(ui_lookup_widget(ui_widgets.prefs_dialog, "problemid_entry1"))),da);
-//fputc(' ',da);
-//fprintf(da,"%d",gtk_combo_box_get_active(GTK_COMBO_BOX(ui_lookup_widget(ui_widgets.prefs_dialog, "language_box2"))));
+if(!da)
+{
+fprintf(da, "%s %s %s %d", 
+  gtk_entry_get_text(GTK_ENTRY(user_entry)),
+  gtk_entry_get_text(GTK_ENTRY(pwd_entry)),
+  gtk_entry_get_text(GTK_ENTRY(probid_entry)),
+  gtk_combo_box_get_active(GTK_COMBO_BOX(language_box)));
+
+fclose(da); 
+}
+else
+{
+	dialogs_show_msgbox(GTK_MESSAGE_INFO, "Data File could not be opened!");
+	//print an error mesage and then 
+	return;
+}
 }
 
 
 static void spoj_submit_online_doit(GtkWidget *submit_button, gpointer gdata)
-{gchar buffer[50];
-yof=document_get_current(); 
-gchar *a,*b,*c,*d;
-gchar *lang;
-lang=(gchar*)malloc(sizeof(gchar)*3);
-a=(gchar*)malloc(sizeof(gchar)*20);
-b=(gchar*)malloc(sizeof(gchar)*20);
-c=(gchar*)malloc(sizeof(gchar)*20);
-d=(gchar*)malloc(sizeof(gchar)*20);
-int i;
-gchar *expression=(gchar*)malloc(sizeof(gchar)*1000);
-a=yof->file_name;
-//msgwin_compiler_add_string(COLOR_BLACK,"fuck yeah!");
+{
+gchar buffer[50];
+soln_doc=document_get_current(); 
+if(!soln_doc)
+{
+gchar *doc_fn, *user_ent, *pwd_ent, *probid_ent,*lang;
+
+lang=malloc(sizeof(gchar)*3);
+
+int active_choice;
+//gchar *expression=malloc(sizeof(gchar)*1000);
+doc_fn=soln_doc->file_name;
 
 
-strcpy(b,gtk_entry_get_text(GTK_ENTRY(user_entry)));
-strcpy(c,gtk_entry_get_text(GTK_ENTRY(pwd_entry)));
-strcpy(d,gtk_entry_get_text(GTK_ENTRY(probid_entry)));
-i=gtk_combo_box_get_active(GTK_COMBO_BOX(language_box));
-msgwin_compiler_add_string(COLOR_BLACK,c);
-switch(i)
+user_ent=g_strdup(gtk_entry_get_text(GTK_ENTRY(user_entry)));
+pwd_ent=g_strdup(gtk_entry_get_text(GTK_ENTRY(pwd_entry)));
+probid_ent=g_strdup(gtk_entry_get_text(GTK_ENTRY(probid_entry)));
+
+
+active_choice=gtk_combo_box_get_active(GTK_COMBO_BOX(language_box));
+
+switch(active_choice)
 {
 case 1:{strcpy(lang,"11");break;}//c
 case 2:{strcpy(lang,"41");break;}//c++
 case 3:{strcpy(lang,"10");break;}//python
 case 4:{strcpy(lang,"12");break;}//brainfuck
-}
-//msgwin_compiler_add_string(COLOR_BLACK,lang);
-expression=strcpy(expression,"python /home/mj/code/y.py ");
-//the path where the python script which does the submission
-//is present 
-strcat(expression,b);
-strcat(expression," ");
-strcat(expression,c);
-strcat(expression," ");
-strcat(expression,a);
-strcat(expression," ");
-strcat(expression,lang);
-strcat(expression," ");
-strcat(expression,d);
+};
+//presently built only for probids c,c++,python and brainfuck will extend it later
+GString *expression=gstring_new("python /home/mj/code/y.py ");
+g_string_append(expression,user_ent);
+g_string_append(expression," ");
+g_string_append(expression,pwd_ent);
+g_string_append(expression," ");
+g_string_append(expression,doc_fn);
+g_string_append(expression," ");
+g_string_append(expression,lang);
+g_string_append(expression," ");
+g_string_append(expression,probid_ent);
 
-FILE* in;
-extern FILE *popen();
-if(!(in = popen((const gchar*)(expression), "r")))
-    {return;}
+FILE* in=popen(expression->str, "r");
+if(!in )
+    {//display a dialog box showing the error
+		dialogs_show_msgbox(GTK_MESSAGE_INFO, "Terminal Pipe could not be opened!");
+		return;
+	}
  
     while(fgets(buffer, 50, in) != NULL) {
     
     		msgwin_compiler_add_string(COLOR_BLACK,(buffer));}
 pclose(in);
- 		
 
+//freeing all the pointers
+g_free(doc_fn);
+g_free(user_ent);
+g_free(pwd_ent);
+g_free(probid_ent);
+g_free(lang);
+}
+else 
+{
+	dialogs_show_msgbox(GTK_MESSAGE_INFO, "Solution file could not be found!");
+//print a error dialog box
+return;
+}
 }
 
 void plugin_init(GeanyData *data)
@@ -131,7 +156,7 @@ void plugin_init(GeanyData *data)
   gtk_window_set_default_size(GTK_WINDOW(window), 100,100);
   gtk_window_set_title (GTK_WINDOW (window), "SPOJ Plugin");
 
-    main_menu_item = gtk_menu_item_new_with_mnemonic("Hello Wor");
+    main_menu_item = gtk_menu_item_new_with_mnemonic("SPOJ");
     gtk_widget_show(main_menu_item);
     gtk_container_add(GTK_CONTAINER(geany->main_widgets->tools_menu),
         main_menu_item);
@@ -192,5 +217,6 @@ void plugin_init(GeanyData *data)
 
 void plugin_cleanup(void)
 {	
+    gtk_widget_destroy(window);
     gtk_widget_destroy(main_menu_item);
 }
