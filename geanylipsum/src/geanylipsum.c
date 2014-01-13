36e85e4176781eddc1b2082705b7b2e6f50e1e0d
Frank Lanitz <frank@frank.uvena.de>
2014-01-13 18:20:26 +0100

    GeanyLipsum: Fix Loremipsum text

--- geanylipsum/src/geanylipsum.c
+++ geanylipsum/src/geanylipsum.c
@@ -45,10 +45,13 @@ PLUGIN_SET_TRANSLATABLE_INFO(
 static GtkWidget *main_menu_item = NULL;
 static gchar *lipsum = NULL;
 static const gchar *default_loremipsum = "\
-Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy\
-eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam\
-voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet \
-clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. ";
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do\
+eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim\
+ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut\
+aliquip ex ea commodo consequat. Duis aute irure dolor in\
+reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla\
+pariatur. Excepteur sint occaecat cupidatat non proident, sunt in\
+culpa qui officia deserunt mollit anim id est laborum.";
 
 
 /* Doing some basic keybinding stuff */
