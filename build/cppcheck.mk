f26710b7e904f713e3fba860c62ebad11a5206ad
Colomban Wendling <ban@herbesfolles.org>
2014-02-21 16:28:08 +0100

    cppcheck: allow for extra user and developer flags
    Make cppheck calls use flags variables `$(AM_CPPCHECKFLAGS)` (for
    developer flags) and `$(CPPCHECKFLAGS)` (for user flags).
    
    This allows both a developer to add specific flags needed to cppcheck
    to behave appropriately for the checked code, as well as the users to
    specify flags, like e.g. the number ob concurrent jobs.

--- build/cppcheck.mk
+++ build/cppcheck.mk
@@ -2,7 +2,10 @@
 if HAVE_CPPCHECK
 
 check-cppcheck: $(srcdir)
-	$(CPPCHECK) -q --template gcc --error-exitcode=2 $^
+	$(CPPCHECK) \
+		-q --template gcc --error-exitcode=2 \
+		$(AM_CPPCHECKFLAGS) $(CPPCHECKFLAGS) \
+		$^
 
 check-local: check-cppcheck
 
