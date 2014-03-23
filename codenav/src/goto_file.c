93208391812ec17a071f8087fff8bd025f1c6c34
federeghe <federico.dev@reghe.net>
2014-03-23 23:02:22 +0100

    Reduced spacing and add changelog

--- codenav/src/goto_file.c
+++ codenav/src/goto_file.c
@@ -1,24 +1,22 @@
 /*
- *	  goto_file.c - this file is part of "codenavigation", which is
- *	  part of the "geany-plugins" project.
+ *  goto_file.c - this file is part of "codenavigation", which is
+ *  part of the "geany-plugins" project.
  *
- *	  Copyright 2009 Lionel Fuentes <funto66(at)gmail(dot)com>
- *	  Copyright 2014 Federico Reghenzani <federico(dot)dev(at)reghe(dot)net>
+ *  Copyright 2009 Lionel Fuentes <funto66(at)gmail(dot)com>
+ *  Copyright 2014 Federico Reghenzani <federico(dot)dev(at)reghe(dot)net>
  * 
- *	  This program is free software; you can redistribute it and/or modify
- *	  it under the terms of the GNU General Public License as published by
- *	  the Free Software Foundation; either version 2 of the License, or
- *	  (at your option) any later version.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
  *
- *	  This program is distributed in the hope that it will be useful,
- *	  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	  GNU General Public License for more details.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- *	  You should have received a copy of the GNU General Public License
- *	  along with this program; if not, write to the Free Software
- *	  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- *	  MA 02110-1301, USA.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifdef HAVE_CONFIG_H
