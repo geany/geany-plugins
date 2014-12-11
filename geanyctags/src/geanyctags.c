be078c2089661fbc090a40e77b93f15355a7db5d
Jiří Techet <techet@gmail.com>
2014-12-11 21:59:10 +0100

    geanyctags: Show help from the plugin manager

--- geanyctags/src/geanyctags.c
+++ geanyctags/src/geanyctags.c
@@ -110,6 +110,10 @@ PluginCallback plugin_callbacks[] = {
 	{NULL, NULL, FALSE, NULL}
 };
 
+void plugin_help (void)
+{
+	utils_open_browser (DOCDIR "/" PLUGIN "/README");
+}
 
 static void spawn_cmd(const gchar *cmd, const gchar *dir)
 {
