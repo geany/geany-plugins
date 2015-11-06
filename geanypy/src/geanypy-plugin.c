0b28be76ffe5791ca4a6b99f16ddb94724b5b76b
Thomas Martitz <kugel@rockbox.org>
2015-11-06 23:56:45 +0100

    geanypy: add support for keybindings.
    geany.Plugin gains two methods to create groups and items

--- geanypy/src/geanypy-plugin.c
+++ geanypy/src/geanypy-plugin.c
@@ -397,8 +397,90 @@ geany_load_module(GeanyPlugin *plugin)
 
 static void PluginBase_dealloc(GeanyPyPluginBase *self) { }
 
+
+static gboolean call_key(gpointer *unused, guint key_id, gpointer data)
+{
+	PyObject *callback = data;
+	PyObject *args;
+
+	args = Py_BuildValue("(i)", key_id);
+	PyObject_CallObject(callback, args);
+	Py_DECREF(args);
+}
+
+
+static PyObject *
+PluginBase_set_kb_group(GeanyPyPluginBase *self, PyObject *args, PyObject *kwargs)
+{
+	static gchar *kwlist[] = { "section_name", "count", "callback", NULL };
+	int count = 0;
+	const gchar *section_name = NULL;
+	GeanyKeyGroup *group = NULL;
+	PyObject *py_callback = NULL;
+	if (PyArg_ParseTupleAndKeywords(args, kwargs, "si|O", kwlist, &section_name, &count, &py_callback))
+	{
+		if (PyCallable_Check(py_callback))
+		{
+			Py_INCREF(py_callback);
+			group = plugin_set_key_group_full(self->plugin, section_name, count,
+			                                  (GeanyKeyGroupFunc) call_key, py_callback, Py_DecRef);
+		}
+		else
+			group = plugin_set_key_group(self->plugin, section_name, count, NULL);
+	}
+
+	if (group)
+	{
+		GObject *wrapper;
+		PyObject *ret;
+		wrapper = g_object_new(G_TYPE_OBJECT, NULL);
+		g_object_set_data(wrapper, "pointer", group);
+		ret = pygobject_new(wrapper);
+		g_object_unref(wrapper);
+		return ret;
+	}
+	Py_RETURN_NONE;
+}
+
+
+static PyObject *
+PluginBase_set_kb_item(GeanyPyPluginBase *self, PyObject *args, PyObject *kwargs)
+{
+	static gchar *kwlist[] = { "key_group", "key_id", "key", "mod", "name", "label", "menu_item", "callback", NULL };
+	int id = -1;
+	int key;
+	int mod;
+	PyObject *py_group;
+	const gchar *name = NULL, *label = NULL;
+	PyObject *py_menu_item = NULL;
+	PyObject *py_callback  = NULL;
+
+	if (PyArg_ParseTupleAndKeywords(args, kwargs, "Oiiiss|OO", kwlist,
+		&py_group, &id, &key, &mod, &name, &label, &py_menu_item, &py_callback) && id >= 0)
+	{
+		GObject   *group_wrapper = G_OBJECT(pygobject_get(py_group));
+		GtkWidget *menu_item = (py_menu_item == NULL || py_menu_item == Py_None)
+									? NULL : GTK_WIDGET(pygobject_get(py_menu_item));
+		GeanyKeyGroup *group = g_object_get_data(group_wrapper, "pointer");
+		if (PyCallable_Check(py_callback))
+		{
+			Py_INCREF(py_callback);
+			keybindings_set_item_full(group, id, (guint) key, (GdkModifierType) mod, name, label,
+									  menu_item, (GeanyKeyBindingFunc) call_key, py_callback, Py_DecRef);
+		}
+		else
+			keybindings_set_item(group, id, NULL, (guint) key, (GdkModifierType) mod, name, label,
+									  menu_item);
+	}
+	Py_RETURN_NONE;
+}
+
 static PyMethodDef
 PluginBase_methods[] = {
+	{ "set_key_group",				(PyCFunction)PluginBase_set_kb_group,	METH_KEYWORDS,
+		"Sets up a GeanyKeybindingGroup for this plugin." },
+	{ "set_key_item",				(PyCFunction)PluginBase_set_kb_item,	METH_KEYWORDS,
+		"Adds an action to one of the plugin's key groups" },
 	{ NULL }
 };
 
