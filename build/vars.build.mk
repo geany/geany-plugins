69af4d80499424cbae03cf1cc18123eb147e369a
Colomban Wendling <ban@herbesfolles.org>
2016-03-12 19:58:39 +0100

    Try and reduce code duplication while simplifying altering paths

--- build/vars.build.mk
+++ build/vars.build.mk
@@ -1,32 +1,36 @@
 if MINGW
-LOCAL_AM_CFLAGS = \
-	-DLOCALEDIR=\""share/locale"\" \
-	-DPREFIX=\"\" \
-	-DDOCDIR=\""share/doc/$(PACKAGE)"\" \
-	-DGEANYPLUGINS_DATADIR=\""share"\" \
-	-DPKGDATADIR=\""share/$(PACKAGE)"\" \
-	-DLIBDIR=\""lib"\" \
-	-DPKGLIBDIR=\""lib/$(PACKAGE)"\" \
-	-DPLUGINDATADIR=\""share/$(PACKAGE)/$(plugin)"\" \
-	-DPLUGINDOCDIR=\""$(plugin)"\" \
-	-DPLUGINLIBDIR=\""lib/$(PACKAGE)/$(plugin)"\"
+GP_PREFIX				= .
+GP_DATADIR				= $(GP_PREFIX)/share
+GP_LOCALEDIR			= $(GP_DATADIR)/locale
+GP_DOCDIR				= $(GP_DATADIR)/doc/$(PACKAGE)
+GP_PKGDATADIR			= $(GP_DATADIR)/$(PACKAGE)
+GP_LIBDIR				= $(GP_PREFIX)/lib
+GP_PKGLIBDIR			= $(GP_LIBDIR)/$(PACKAGE)
 else
-LOCAL_AM_CFLAGS = \
-	-DLOCALEDIR=\""$(LOCALEDIR)"\" \
-	-DPREFIX=\""$(prefix)"\" \
-	-DDOCDIR=\""$(docdir)"\" \
-	-DGEANYPLUGINS_DATADIR=\""$(datadir)"\" \
-	-DPKGDATADIR=\""$(pkgdatadir)"\" \
-	-DLIBDIR=\""$(libdir)"\" \
-	-DPKGLIBDIR=\""$(pkglibdir)"\" \
-	-DPLUGINDATADIR=\""$(pkgdatadir)/$(plugin)"\" \
-	-DPLUGINDOCDIR=\""$(docdir)/$(plugin)"\" \
-	-DPLUGINLIBDIR=\""$(pkglibdir)/$(plugin)"\"
+GP_PREFIX				= $(prefix)
+GP_DATADIR				= $(datadir)
+GP_LOCALEDIR			= $(LOCALEDIR)
+GP_DOCDIR				= $(docdir)
+GP_PKGDATADIR			= $(pkgdatadir)
+GP_LIBDIR				= $(libdir)
+GP_PKGLIBDIR			= $(pkglibdir)
 endif
+
+LOCAL_AM_CFLAGS = \
+	-DLOCALEDIR=\""$(GP_LOCALEDIR)"\" \
+	-DPREFIX=\""$(GP_PREFIX)"\" \
+	-DDOCDIR=\""$(GP_DOCDIR)"\" \
+	-DGEANYPLUGINS_DATADIR=\""$(GP_DATADIR)"\" \
+	-DPKGDATADIR=\""$(GP_PKGDATADIR)"\" \
+	-DLIBDIR=\""$(GP_LIBDIR)"\" \
+	-DPKGLIBDIR=\""$(GP_PKGLIBDIR)"\" \
+	-DPLUGINDATADIR=\""$(GP_PKGDATADIR)/$(plugin)"\" \
+	-DPLUGINDOCDIR=\""$(GP_DOCDIR)/$(plugin)"\" \
+	-DPLUGINLIBDIR=\""$(GP_PKGLIBDIR)/$(plugin)"\" \
+	-DPLUGIN="\"$(plugin)\""
 
 AM_CFLAGS = \
 	${LOCAL_AM_CFLAGS} \
-	-DPLUGIN="\"$(plugin)\"" \
 	$(GEANY_CFLAGS) \
 	$(GP_CFLAGS)
 
