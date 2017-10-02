759f1fe2fab7350ab35dfc898c4e87ee3c76b1be
LarsDW223 <lars_paulsen@web.de>
2017-10-02 19:24:47 +0200

    geanylua: Replace deprecated tm_get_real_path().

--- geanylua/gsdlg.c
+++ geanylua/gsdlg.c
@@ -106,7 +106,7 @@ static void file_btn_clicked(GtkButton *button, gpointer user_data)
 	fn=gtk_entry_get_text(GTK_ENTRY(user_data));
 	if (fn && *fn) {
 		if (g_file_test(fn,G_FILE_TEST_IS_REGULAR)) {
-			gchar *rp=tm_get_real_path(fn);
+			gchar *rp=utils_get_real_path(fn);
 			gtk_file_chooser_set_filename(GTK_FILE_CHOOSER(dlg), rp);
 			if (rp) g_free(rp);
 		} else {
@@ -115,7 +115,7 @@ static void file_btn_clicked(GtkButton *button, gpointer user_data)
 			} else {
 				gchar *dn=g_path_get_dirname(fn);
 				if (g_file_test(dn,G_FILE_TEST_IS_DIR)) {
-					gchar *rp=tm_get_real_path(dn);
+					gchar *rp=utils_get_real_path(dn);
 					gtk_file_chooser_set_current_folder(GTK_FILE_CHOOSER(dlg), rp);
 					if (rp) g_free(rp);
 					bn=g_path_get_basename(fn);
