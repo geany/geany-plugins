ea987b5a837889763f5f47b0141bac9b49e61fb5
Lars Paulsen <lars_paulsen@web.de>
2018-06-18 21:38:00 +0200

    addons: new plugin API requires version 1.26 (226)

--- addons/src/addons.c
+++ addons/src/addons.c
@@ -739,5 +739,5 @@ void geany_load_module(GeanyPlugin *plugin)
 	plugin->funcs->callbacks = plugin_addons_callbacks;
 
 	/* Register! */
-	GEANY_PLUGIN_REGISTER(plugin, 224);
+	GEANY_PLUGIN_REGISTER(plugin, 226);
 }
