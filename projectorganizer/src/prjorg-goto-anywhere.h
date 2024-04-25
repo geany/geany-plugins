60b4c81335ca239f8e4d15c2a58e99055b020531
Jiří Techet <techet@gmail.com>
2024-04-25 23:46:23 +0200

    projectorganizer: Add popup panel for navigation
    This feature allows quick navigation do document/workspace symbols
    based on their names, open files, and line numbers.
    
    The panel's code is mostly stolen from the LSP plugin which in turn
    stole it from the Colomban Wendling's Commander plugin.

---
+++ projectorganizer/src/prjorg-goto-anywhere.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2023 Jiri Techet <techet@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef PRJORG_GOTO_ANYWHERE_H
+#define PRJORG_GOTO_ANYWHERE_H 1
+
+
+void prjorg_goto_anywhere_for_workspace(void);
+void prjorg_goto_anywhere_for_doc(void);
+void prjorg_goto_anywhere_for_line(void);
+void prjorg_goto_anywhere_for_file(void);
+
+#endif  /* PRJORG_GOTO_ANYWHERE_H */
