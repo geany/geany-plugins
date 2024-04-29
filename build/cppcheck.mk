95570a640e9bbd3e0802f1946e8c67ddec9d383c
Colomban Wendling <ban@herbesfolles.org>
2024-04-29 23:01:58 +0200

    cppcheck: Properly define GETTEXT_PACKAGE globally
    Scope plugin defined it to an incorrect value (the quotes were missing)
    and it's likely useful for all code in the repository.

--- build/cppcheck.mk
+++ build/cppcheck.mk
@@ -6,6 +6,7 @@ check-cppcheck: $(srcdir)
 		-q --template=gcc --error-exitcode=2 \
 		--library=$(top_srcdir)/build/cppcheck-geany-plugins.cfg \
 		-I$(GEANY_INCLUDEDIR)/geany \
+		-DGETTEXT_PACKAGE=\"$(GETTEXT_PACKAGE)\" \
 		$(filter -j%,$(MAKEFLAGS)) \
 		$(AM_CPPCHECKFLAGS) $(CPPCHECKFLAGS) \
 		$(srcdir)
