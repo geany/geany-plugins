45f40178484291a7cadda1a018cff01678d59bf4
Jiří Techet <techet@gmail.com>
2025-06-25 10:37:22 +0200

    webhelper: Remove deprecated ui_frame_new_with_alignment()
    The patch basically just copies the implementation of
    ui_frame_new_with_alignment() to the plugin.

--- webhelper/src/gwh-plugin.c
+++ webhelper/src/gwh-plugin.c
@@ -507,6 +507,32 @@ on_configure_dialog_response (GtkDialog        *dialog,
 }
 
 static GtkWidget *
+frame_new_with_alignment (const gchar  *label_text,
+                          GtkWidget   **alignment)
+{
+  GtkWidget  *label;
+  GtkWidget  *align;
+  GtkWidget  *frame = gtk_frame_new (NULL);
+  gchar      *text  = g_strconcat ("<b>", label_text, "</b>", NULL);
+  
+  gtk_frame_set_shadow_type (GTK_FRAME (frame), GTK_SHADOW_NONE);
+  
+  align = gtk_alignment_new (0.5, 0.5, 1, 1);
+  gtk_container_add (GTK_CONTAINER (frame), align);
+  gtk_alignment_set_padding (GTK_ALIGNMENT (align), 0, 0, 12, 0);
+  
+  label = gtk_label_new (NULL);
+  gtk_label_set_text (GTK_LABEL (label), text);
+  gtk_label_set_use_markup (GTK_LABEL (label), TRUE);
+  gtk_frame_set_label_widget (GTK_FRAME (frame), label);
+  
+  *alignment = align;
+  g_free (text);
+  
+  return frame;
+}
+
+GtkWidget *
 gwh_plugin_configure (GeanyPlugin  *plugin,
                       GtkDialog    *dialog,
                       gpointer      data)
@@ -522,7 +548,7 @@ gwh_plugin_configure (GeanyPlugin  *plugin,
   box1 = gtk_box_new (GTK_ORIENTATION_VERTICAL, 12);
   
   /* Browser */
-  gtk_box_pack_start (GTK_BOX (box1), ui_frame_new_with_alignment (_("Browser"), &alignment), FALSE, FALSE, 0);
+  gtk_box_pack_start (GTK_BOX (box1), frame_new_with_alignment (_("Browser"), &alignment), FALSE, FALSE, 0);
   box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
   gtk_container_add (GTK_CONTAINER (alignment), box);
   /* browser position */
@@ -534,7 +560,7 @@ gwh_plugin_configure (GeanyPlugin  *plugin,
   gtk_box_pack_start (GTK_BOX (box), cdialog->browser_auto_reload, FALSE, TRUE, 0);
   
   /* Windows */
-  gtk_box_pack_start (GTK_BOX (box1), ui_frame_new_with_alignment (_("Windows"), &alignment), FALSE, FALSE, 0);
+  gtk_box_pack_start (GTK_BOX (box1), frame_new_with_alignment (_("Windows"), &alignment), FALSE, FALSE, 0);
   box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
   gtk_container_add (GTK_CONTAINER (alignment), box);
   /* skip taskbar */
